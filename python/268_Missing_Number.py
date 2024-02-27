fb72486a95f54cd49711600dc7e304c2d2ceffd2
Den Kom <deniss.komarskis@gmail.com>
2024-02-27 21:05:14 +0200

    solution: 268

---
+++ python/268_Missing_Number.py
@@ -0,0 +1,54 @@
+"""
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Constraints:
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+ 
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+"""
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # n = len(nums)
+        
+        # all_numbers = [0 for _ in range(0, n + 1)]
+        
+        # for num in nums:
+        #     all_numbers[num] = 1
+            
+        # for i in range(n + 1):
+        #     if all_numbers[i] == 0:
+        #         return i
+        
+        n = len(nums)
+        
+        total_sum = n*(n+1) // 2
+        
+        return total_sum - sum(nums)
+        
+        
+
+"""
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+"""
+
+print(Solution().missingNumber([3,0,1]))
+print(Solution().missingNumber([0,1]))
+print(Solution().missingNumber([9,6,4,2,3,5,7,0,1]))
